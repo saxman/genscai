51e535c307acf16b5bff678f78a8263105c544cc
Katherine Rosenfeld <krosenf@gmail.com>
2024-12-25 03:11:37 +0000

    ruff

--- scripts/01_retrieve_information.py
+++ scripts/01_retrieve_information.py
@@ -1,13 +1,16 @@
 """
-This script retrieves MIDAS abstracts and metadata using a daterange (inclusive) 
+This script retrieves MIDAS abstracts and metadata using a daterange (inclusive)
 and saves them to a database.
 """
+
 import genscai
 
-OUTPUT_PATH = genscai.paths.data / 'raw'
+OUTPUT_PATH = genscai.paths.data / "raw"
 
 # Create and run a retriever ($$)
 for year in range(2020, 2024):
     print(f"Retrieving articles for {year}")
-    retriever = genscai.retrieval.MIDASRetriever(startdate=f'{year}-01-01', enddate=f'{year}-12-31', database_path=OUTPUT_PATH)
+    retriever = genscai.retrieval.MIDASRetriever(
+        startdate=f"{year}-01-01", enddate=f"{year}-12-31", database_path=OUTPUT_PATH
+    )
     retriever()

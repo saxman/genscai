c7ec03044cdfdca726440f34ccf0d97fb1fa3078
Katherine Rosenfeld <krosenf@gmail.com>
2024-12-25 03:10:06 +0000

    Add docstring and set output path for MIDASRetriever database

--- scripts/01_retrieve_information.py
+++ scripts/01_retrieve_information.py
@@ -1,7 +1,13 @@
+"""
+This script retrieves MIDAS abstracts and metadata using a daterange (inclusive) 
+and saves them to a database.
+"""
 import genscai
 
-# Create a retriever object
+OUTPUT_PATH = genscai.paths.data / 'raw'
+
+# Create and run a retriever ($$)
 for year in range(2020, 2024):
     print(f"Retrieving articles for {year}")
-    retriever = genscai.retrieval.MIDASRetriever(startdate=f'{year}-01-01', enddate=f'{year}-12-31')
+    retriever = genscai.retrieval.MIDASRetriever(startdate=f'{year}-01-01', enddate=f'{year}-12-31', database_path=OUTPUT_PATH)
     retriever()

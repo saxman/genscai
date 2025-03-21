7d89f6c50375725900fa8fcc2b9e875c2f78bbf5
Paul R Saxman <saxman@gmail.com>
2025-03-21 11:10:02 -0700

    moved DeepSeek evaluation to proper script

--- scripts/04_classification_cross_model_validation.py
+++ scripts/04_classification_cross_model_validation.py
@@ -6,9 +6,16 @@ from genscai.data import load_classification_training_data
 from genscai.prompts import PromptCatalog
 import genscai.classification as gc
 
-
 logger = logging.getLogger(__name__)
 logging.getLogger("httpx").setLevel(logging.WARNING)
+
+CLASSIFICATION_GENERATE_KWARGS = gc.CLASSIFICATION_GENERATE_KWARGS.copy()
+CLASSIFICATION_GENERATE_KWARGS.update(
+    {
+        "max_new_tokens": 1024,
+        "temperature": 0.70,
+    }
+)
 
 
 def run_tests():
@@ -27,6 +34,9 @@ def run_tests():
     # only test models that are local Hugging Face models, which are identified by having a "/" in the model ID
     test_model_ids = [x for x in prompt_model_ids if "/" in x]
 
+    # TODO only running for one model for now
+    test_model_ids = [ModelClient.MODEL_DEEPSEEK_R1_8B]
+
     for test_model_id in test_model_ids:
         print(f"loading model: {test_model_id}", flush=True)
         model_client = ModelClient(test_model_id, MODEL_KWARGS)
@@ -35,7 +45,7 @@ def run_tests():
             prompt = catalog.retrieve_last(prompt_model_id)
             prompt_template = prompt.prompt + gc.CLASSIFICATION_OUTPUT_PROMPT_TEMPLATE
 
-            df_data = gc.classify_papers(model_client, prompt_template, gc.CLASSIFICATION_GENERATE_KWARGS, df_data)
+            df_data = gc.classify_papers(model_client, prompt_template, CLASSIFICATION_GENERATE_KWARGS, df_data)
             df_data, metrics = gc.test_paper_classifications(df_data)
 
             print(f"prompt: {prompt_model_id}, model: {test_model_id}, metrics: {metrics}")
--- scripts/06_classification_all_models.py
+++ scripts/06_classification_all_models.py
@@ -1,30 +1,25 @@
 import pandas as pd
+
 from genscai.models import HuggingFaceClient, MODEL_KWARGS
-from genscai.classification import CLASSIFICATION_OUTPUT_PROMPT_TEMPLATE
-import genscai.classification as gc
+from genscai.classification import CLASSIFICATION_OUTPUT_PROMPT_TEMPLATE, CLASSIFICATION_GENERATE_KWARGS
 from genscai import paths
 from genscai.classification import classify_papers
 from genscai.data import load_midas_data
 from genscai.prompts import PromptCatalog
 
 MODEL_IDS = [
-    # HuggingFaceClient.MODEL_LLAMA_3_1_8B,
-    # HuggingFaceClient.MODEL_GEMMA_2_9B,
-    # HuggingFaceClient.MODEL_PHI_4_14B,
-    # HuggingFaceClient.MODEL_MISTRAL_NEMO_12B,
-    HuggingFaceClient.MODEL_DEEPSEEK_R1_8B
+    HuggingFaceClient.MODEL_LLAMA_3_1_8B,
+    HuggingFaceClient.MODEL_GEMMA_2_9B,
+    HuggingFaceClient.MODEL_PHI_4_14B,
+    HuggingFaceClient.MODEL_MISTRAL_NEMO_12B,
 ]
-
-CLASSIFICATION_GENERATE_KWARGS = gc.CLASSIFICATION_GENERATE_KWARGS.copy()
-CLASSIFICATION_GENERATE_KWARGS.update(
-    {
-        "max_new_tokens": 1024,
-        "temperature": 0.70,
-    }
-)
 
 
 def run_classification():
+    """
+    Runs the classification process for all models specified in MODEL_IDS.
+    Results are saved to JSON files named `modeling_papers_{i}.json` in the project data directory.
+    """
     df_train, df_test, df_validate = load_midas_data()
     catalog = PromptCatalog(paths.data / "prompt_catalog.db")
 
@@ -42,7 +37,7 @@ def run_classification():
         )
 
         df = df.query("predict_modeling == True")
-        df.to_json(paths.data / f"modeling_papers_{i + 4}.json", orient="records", lines=True)
+        df.to_json(paths.data / f"modeling_papers_{i}.json", orient="records", lines=True)
 
         del model_client
 

0b8cf8ad9bcadb23552a93bff08bf82c91de6f33
Paul R. Saxman <saxman@gmail.com>
2025-05-30 13:48:37 -0700

    feat: completed tool usage for research articles

--- streamlit/chatbot_with_tools_ollama.py
+++ streamlit/chatbot_with_tools_ollama.py
@@ -6,32 +6,33 @@ from genscai.models import OllamaClient as ModelClient
 import torch
 import chromadb
 
-# Avoid torch RuntimeError
+# Avoid torch RuntimeError when using Hugging Face Transformers
 torch.classes.__path__ = []
 
 MODEL_ID = ModelClient.MODEL_MISTRAL_SMALL_3_1_24B
-KNOWLEDGE_BASE_ID = "medrxiv_chunked_256_cosine"
+
+KNOWLEDGE_BASE_PATH = str(paths.output / "medrxiv.db")
+KNOWLEDGE_BASE_ID = "articles_cosign_chunked_256"
 
 SYSTEM_MESSAGE = """
 You are a friendly chatbot that assists the user with research in infectious diseases and infectious disease modeling.
 Reply in short, concise sentrences, unless the user asks for a more detailed answer.
-Do not provide references in your response, unless the user specifically asks for them.
+Always provide links to the articles you reference.
 Please introduce yourself.
 """
 
-def get_research_articles(search_request: str) -> list[dict]:
+
+def search_research_articles(search_request: str) -> tuple[str, list[dict]]:
     """
-    Get the current research in infectious diseases and disease modeling per a given search request.
+    Search for current research articles in infectious diseases and disease modeling per a given search request.
 
     Args:
         search_request: The information that the user is looking for.
     Returns:
-        Current research in infectious diseases and disease modeling for the given topic.
+        Current research articles on infectious diseases and disease modeling for the given topic.
     """
 
-    print(f"Search request: {search_request}")
-
-    client = chromadb.PersistentClient(path=str(paths.output / "genscai.db"))
+    client = chromadb.PersistentClient(path=KNOWLEDGE_BASE_PATH)
     collection = client.get_collection(name=KNOWLEDGE_BASE_ID)
     results = collection.query(query_texts=[search_request], n_results=10)
 
@@ -40,8 +41,9 @@ def get_research_articles(search_request: str) -> list[dict]:
     metadata = [x for x in results["metadatas"][0]]
 
     # Each article has multiple chunks, and each chunk id is the article's doi with an index appended to it.
-    # Therefore, we need to remove the index from the id and keep only one copy of the article.
+    # To get a unique set of articles, we need to remove the index from the id and keep only one copy of article doi.
     articles = []
+    content = "Relevant research articles:\n\n"
     id_set = set()
     for i in range(len(ids)):
         id = ids[i].split(":")[0]
@@ -51,16 +53,18 @@ def get_research_articles(search_request: str) -> list[dict]:
 
         id_set.add(id)
         articles.append({"id": id, "abstract": abstracts[i], "metadata": metadata[i]})
+        content += f"Title: {metadata[i]['title']}\nAbstract: {abstracts[i]}\nAuthors: {metadata[i]['authors']}\nDate: {metadata[i]['date']}\nURL: https://www.medrxiv.org/content/{id}\n\n"
 
-    return articles
+    return content, articles
 
-MODEL_TOOLS = [get_research_articles]
+
+MODEL_TOOLS = [search_research_articles]
 
 with st.sidebar:
     st.title("IDM Research Assistant")
     st.write("Discuss infectious disease modeling with access to current disease modeling research.")
 
-    temperature = st.sidebar.slider("temperature", min_value=0.01, max_value=1.0, value=0.1, step=0.01)
+    temperature = st.sidebar.slider("temperature", min_value=0.01, max_value=1.0, value=0.15, step=0.01)
     top_p = st.sidebar.slider("top_p", min_value=0.01, max_value=1.0, value=0.9, step=0.01)
 
 # Initialize the session state if we don't already have a model loaded
@@ -68,7 +72,9 @@ if "model_client" not in st.session_state:
     messages = [{"role": "system", "content": SYSTEM_MESSAGE}]
     model_client = ModelClient(model_id=MODEL_ID)
 
-    st.session_state.messages = model_client.chat(messages)
+    st.session_state.messages = model_client.chat(
+        messages, generate_kwargs={"temperature": temperature, "top_p": top_p}
+    )
     st.session_state.model_client = model_client
 
 # Only render assistant and user messages (not tool messages)
@@ -86,27 +92,21 @@ if prompt := st.chat_input("What's up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
     model_client = st.session_state.model_client
-    messages = model_client.chat(st.session_state.messages, tools=MODEL_TOOLS)
-
-    print(messages[-1])
+    messages = model_client.chat(
+        st.session_state.messages, tools=MODEL_TOOLS, generate_kwargs={"temperature": temperature, "top_p": top_p}
+    )
 
     if "tool_calls" in messages[-1]:
-        tool_calls = messages[-1]["tool_calls"]
-
-        for tool_call in tool_calls:
+        for tool_call in messages[-1]["tool_calls"]:
             tool_name = tool_call["function"]["name"]
             tool_args = tool_call["function"]["arguments"]
-            content = ""
 
-            # Call the tool function
-            if tool_name == "get_research_articles":
-                articles = get_research_articles(**tool_args)
-                for article in articles:
-                    content += f"- **{article['id']}**: {article['abstract']}\n"
+            content, data = globals()[tool_name](**tool_args)
 
-            # Add the tool response to the message history and send it back to the model
+            # Add the tool response to the message history and send it back to the model to generate a final response
             messages.append({"role": "tool", "content": content})
-            messages = model_client.chat(messages)
+
+        messages = model_client.chat(messages, generate_kwargs={"temperature": temperature, "top_p": top_p})
 
     st.session_state.messages = messages
 

de890e190e1284a431682256d5efd6f2b030ec62
Paul R. Saxman <saxman@gmail.com>
2025-06-09 11:34:32 -0700

    feat: add tools module for encapsulating llm/agent tools

---
+++ genscai/tools.py
@@ -0,0 +1,42 @@
+from genscai import paths
+
+import chromadb
+
+KNOWLEDGE_BASE_PATH = str(paths.output / "medrxiv.db")
+KNOWLEDGE_BASE_ID = "articles_cosign_chunked_256"
+
+def search_research_articles(search_request: str) -> str:
+    """
+    Search for current research articles in infectious diseases and disease modeling per a given search request.
+
+    Args:
+        search_request: The information that the user is looking for.
+
+    Returns:
+        Current research articles on infectious diseases and disease modeling for the given topic.
+    """
+
+    client = chromadb.PersistentClient(path=KNOWLEDGE_BASE_PATH)
+    collection = client.get_collection(name=KNOWLEDGE_BASE_ID)
+    results = collection.query(query_texts=[search_request], n_results=10)
+
+    ids = [x for x in results["ids"][0]]
+    abstracts = [x for x in results["documents"][0]]
+    metadata = [x for x in results["metadatas"][0]]
+
+    # Each article has multiple chunks, and each chunk id is the article's doi with an index appended to it.
+    # To get a unique set of articles, we need to remove the index from the id and keep only one copy of article doi.
+    articles = []
+    content = "Relevant research articles:\n\n"
+    id_set = set()
+    for i in range(len(ids)):
+        id = ids[i].split(":")[0]
+
+        if id in id_set:
+            continue
+
+        id_set.add(id)
+        articles.append({"id": id, "abstract": abstracts[i], "metadata": metadata[i]})
+        content += f"Title: {metadata[i]['title']}\nAbstract: {abstracts[i]}\nAuthors: {metadata[i]['authors']}\nDate: {metadata[i]['date']}\nLink: https://www.medrxiv.org/content/{id}\n\n"
+
+    return content
--- streamlit/genscai_chatbot.py
+++ streamlit/genscai_chatbot.py
@@ -1,13 +1,12 @@
 import streamlit as st
 
-from genscai import paths
 from genscai.models import OllamaClient as ModelClient
+from genscai.tools import search_research_articles
 
-import torch
-import chromadb
 import json
+import torch
 
-# Avoid torch RuntimeError when using Hugging Face Transformers
+# Avoid torch RuntimeError when using Hugging Face Transformers (used by genscai.models)
 torch.classes.__path__ = []
 
 MODELS = [
@@ -16,53 +15,12 @@ MODELS = [
     ModelClient.MODEL_LLAMA_3_2_3B,
 ]
 
-KNOWLEDGE_BASE_PATH = str(paths.output / "medrxiv.db")
-KNOWLEDGE_BASE_ID = "articles_cosign_chunked_256"
-
 SYSTEM_MESSAGE = """
 You are a friendly chatbot that assists the user with research in infectious diseases and infectious disease modeling.
 Reply in short, concise sentrences, unless the user asks for a more detailed answer.
 Always provide links to the articles you reference.
 Please introduce yourself.
 """
-
-
-def search_research_articles(search_request: str) -> tuple[str, list[dict]]:
-    """
-    Search for current research articles in infectious diseases and disease modeling per a given search request.
-
-    Args:
-        search_request: The information that the user is looking for.
-
-    Returns:
-        Current research articles on infectious diseases and disease modeling for the given topic.
-    """
-
-    client = chromadb.PersistentClient(path=KNOWLEDGE_BASE_PATH)
-    collection = client.get_collection(name=KNOWLEDGE_BASE_ID)
-    results = collection.query(query_texts=[search_request], n_results=10)
-
-    ids = [x for x in results["ids"][0]]
-    abstracts = [x for x in results["documents"][0]]
-    metadata = [x for x in results["metadatas"][0]]
-
-    # Each article has multiple chunks, and each chunk id is the article's doi with an index appended to it.
-    # To get a unique set of articles, we need to remove the index from the id and keep only one copy of article doi.
-    articles = []
-    content = "Relevant research articles:\n\n"
-    id_set = set()
-    for i in range(len(ids)):
-        id = ids[i].split(":")[0]
-
-        if id in id_set:
-            continue
-
-        id_set.add(id)
-        articles.append({"id": id, "abstract": abstracts[i], "metadata": metadata[i]})
-        content += f"Title: {metadata[i]['title']}\nAbstract: {abstracts[i]}\nAuthors: {metadata[i]['authors']}\nDate: {metadata[i]['date']}\nLink: https://www.medrxiv.org/content/{id}\n\n"
-
-    return content, articles
-
 
 MODEL_TOOLS = [search_research_articles]
 

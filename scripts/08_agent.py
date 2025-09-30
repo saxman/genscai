898e3c457c847093ce91677b5ef34b28a2081912
Paul R. Saxman <saxman@gmail.com>
2025-09-30 09:44:24 -0700

    formatting

--- scripts/08_agent.py
+++ scripts/08_agent.py
@@ -3,13 +3,11 @@ import ollama
 
 # MODEL = 'ollama_chat/qwen2.5-coder:14b'
 # MODEL = 'command-r7b:7b'
-MODEL = 'qwen3-coder:30b'
+MODEL = "qwen3-coder:30b"
 
 ollama.pull(MODEL)
 
-model = LiteLLMModel(
-    model_id=f'ollama_chat/{MODEL}'
-)
+model = LiteLLMModel(model_id=f"ollama_chat/{MODEL}")
 
 agent = CodeAgent(tools=[WebSearchTool()], model=model, stream_outputs=True)
 
